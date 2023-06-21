6e3a7ad4907de44d7548098874fc22c83b2484e7
Van Hien <hiench2001@gmail.com>
2023-06-21 12:59:18 -0600

    created templates and connect postgredb

--- auth_app/auth_app/settings.py
+++ auth_app/auth_app/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+USER_DB = os.getenv("DB_USER")
+PASS_DB = os.getenv("DB_PASS")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main.apps.MainConfig'
+    
 ]
 
 MIDDLEWARE = [
@@ -75,8 +83,12 @@ WSGI_APPLICATION = 'auth_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'Phanthaonhi01',
+        'HOST': 'database-2.cc3bqs9rarpd.us-west-2.rds.amazonaws.com',
+        'PORT': '5432'
     }
 }
 
